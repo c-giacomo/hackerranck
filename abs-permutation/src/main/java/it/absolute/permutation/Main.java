542ae3eab822a1a0f8300d4ce5be13cbf67018f8
gchiavolotti <giacomo.chiavolotti@pegaso2000.it>
2024-04-16 16:09:22 +0200

    dsf

--- abs-permutation/src/main/java/it/absolute/permutation/Main.java
+++ abs-permutation/src/main/java/it/absolute/permutation/Main.java
@@ -5,7 +5,7 @@ import it.absolute.permutation.core.Solve;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(new Solve().solve(10, 5));
+		System.out.println(new Solve().solve(10, 5)); //
 
 	}
 
